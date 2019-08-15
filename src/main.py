082278998754cfc054c2cabc35edfb51bb27c77e
Gabriel <lesg2605@usherbrooke.ca>
2019-08-15 16:28:12 -0400

    Refactored some names

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from src import TableSorter
+from src import PlayerAnalyser
 
 print("Sorting players...")
 
 data_file = "/home/casto/Documents/Git/Draft_Algorythm/CSVs/TopPlayers.csv"
-players = TableSorter.create_dict_from_file(data_file)
+players = PlayerAnalyser.create_dict_from_file(data_file)
 
-sorted_players = TableSorter.sort_by_key(players, "ExpectedPoints")
+sorted_players = PlayerAnalyser.sort_by_key(players, "ExpectedPoints")
 for item in sorted_players:
 	print(item)
 
